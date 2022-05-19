91262c73e57caea7ae0ca4e30e13cea29e7c72ff
ksendzo <ksenijabulatovi@gmail.com>
2022-05-19 18:52:29 +0200

    promene

--- src/Messaging/KeyRings.java
+++ src/Messaging/KeyRings.java
@@ -3,12 +3,23 @@ package Messaging;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
 
+<<<<<<< Updated upstream
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
+=======
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.*;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+>>>>>>> Stashed changes
 
 public class KeyRings {
 
@@ -27,6 +38,52 @@ public class KeyRings {
     }
 
     public static void generateNewKeyPair(int _userId, long _keyId, String _password){
+
+        KeyPairGenerator kpg = null;
+        try {
+            kpg = KeyPairGenerator.getInstance("DSA", "BC");
+
+            kpg.initialize(1024);
+            KeyPair kp = kpg.generateKeyPair();
+
+            SecureRandom secureRandom = new SecureRandom();
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+
+            keyPairGenerator.initialize(1024, secureRandom);
+
+            KeyPair keyPair =  keyPairGenerator.generateKeyPair();
+            PublicKey publicKey = keyPair.getPublic();
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            PGPKeyPair pair2 = new PGPKeyPair(new PGPKeyPair())
+
+            PGPKeyPair pair = new PGPKeyPair(PGPPublicKey.ELGAMAL_ENCRYPT, kp, new Date(), "BC");
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//        // byte sessionKey[], byte publicKey[]
+//
+//            SecureRandom secureRandom = new SecureRandom();
+//            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+//
+//            keyPairGenerator.initialize(1024, secureRandom);
+//
+//            KeyPair keyPair =  keyPairGenerator.generateKeyPair();
+//
+//            System.out.println("Public Key is: " + keyPair.getPublic().toString());
+//            System.out.println("Privte Key is: " + keyPair.getPrivate().toString());
+//
+//            PrivateKey privateKey = keyPair.getPrivate();
+//            PublicKey myPublicKey = keyPair.getPublic();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+
         //private LocalDate date = LocalDate.now();   //yyyy-mm-dd date of generated keys
         //private KeyStore.PrivateKeyEntry privateKey;         //encrypted private key by IDEA and hashed password
         //private PGPPublicKey publicKey;             //public key
