a67e1084f0a73ac5bfbdd60783ab4e04380ad1eb
Blodhead <81102425+Blodhead@users.noreply.github.com>
2022-05-19 21:49:11 +0200

    GUI start

--- src/App/View_User.java
+++ src/App/View_User.java
@@ -2,27 +2,24 @@ package App;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JTabbedPane;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileSystemView;
+
 public class View_User extends JFrame {
 
-    public static View_User user_view;
-    final static String BUTTONPANEL = "Card with JButtons";
-    final static String TEXTPANEL = "Card with JTextField";
-    JPanel cards;
-    private int currentCard = 0;
-
+    private final JPanel p1 = new JPanel();
+    private final JPanel p2 = new JPanel();
+    private final JPanel p3 = new JPanel();
     private View_User() {
-        super("User_view");
-
+        super("Pretty Good Privacy protocol");
 
         addWindowListener(new WindowAdapter() {
             @Override
@@ -32,8 +29,14 @@ public class View_User extends JFrame {
         });
 
         fill_space();
-        setBounds(200, 100, 1550, 850);
+        setBounds(300, 150, 1300, 800);
+
+        fill_tab1();
+        fill_tab2();
+        fill_tab3();
+
         this.setVisible(true);
+        this.setResizable(false);
     }
 
 private void fill_space() {
@@ -43,14 +46,6 @@ private void fill_space() {
     JTabbedPane tabbedPane = new JTabbedPane();
 
     tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-
-    JPanel p1 = new JPanel();
-    JPanel p2 = new JPanel();
-    JPanel p3 = new JPanel();
-
-    p1.setFont(new Font("Serif", Font.BOLD,24));
-    p2.setFont(new Font("Serif", Font.BOLD,24));
-    p3.setFont(new Font("Serif", Font.BOLD,24));
 
     tabbedPane.setBounds(50, 50, 200, 200);
 
@@ -68,14 +63,48 @@ private void fill_space() {
     j2.setFont(new Font("Serif", Font.BOLD,30));
     j3.setFont(new Font("Serif", Font.BOLD,30));
 
-    p1.add(j1);
-    p2.add(j2);
-    p3.add(j3);
+    p1.setLayout(new BorderLayout());
+    p2.setLayout(new BorderLayout());
+    p3.setLayout(new BorderLayout());
+
+    p1.add(j1,BorderLayout.NORTH);
+    p2.add(j2,BorderLayout.NORTH);
+    p3.add(j3,BorderLayout.NORTH);
 
 }
 
+private void fill_tab1(){
+        //ENKRIPCIJA
 
-    public static View_User getUser_view() {//bice pozvana samo jednom
-        return new View_User();
+    JPanel form = new JPanel(new BorderLayout(2,2));
+
+    JPanel labelFields = new JPanel(new GridLayout(0,2,1,1));
+    labelFields.setBorder(new TitledBorder("GridLayout"));
+    JPanel fields = new JPanel(new GridLayout(0,2,1,1));
+    fields.setBorder(new TitledBorder("GridLayout"));
+
+    for (int ii=1; ii<8; ii++) {
+        labelFields.add(new JLabel("Label " + ii));
+        // if these were of different size, it would be necessary to
+        // constrain them using another panel
+        labelFields.add(new JTextField(10));
+    }
+
+    JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+    //labelFields.add(fields, BorderLayout.EAST);
+
+    form.add(labelFields, BorderLayout.NORTH);
+    form.add(j);
+    //sgui.add(guiCenter, BorderLayout.CENTER);
+
+    p1.add(form, BorderLayout.CENTER);
+
+}
+private void fill_tab2(){}
+private void fill_tab3(){}
+
+    public static void getUser_view() {//bice pozvana samo jednom
+        new View_User();
     }
 }
