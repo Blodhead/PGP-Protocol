2877bbcfa4144f42acd41c078f144c1444819ea4
ksendzo <ksenijabulatovi@gmail.com>
2022-05-20 13:43:28 +0200

    nesto od juce

--- src/Messaging/Admin.java
+++ src/Messaging/Admin.java
@@ -1,24 +1,38 @@
 package Messaging;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import java.security.KeyPairGenerator;
+
+import java.security.*;
+import java.security.cert.CertificateFactory;
 
 public class Admin {
-    private static BouncyCastleProvider provider;
+    private static Provider provider;
     private static Admin admin;
 
     public static _DSAKeyPairGenerator randomKeyGen = new _DSAKeyPairGenerator();
     public static KeyPairGenerator keyPairGen = null;
     private Admin(){
-        this.provider = new BouncyCastleProvider();
+
+        Signature sr = null;
+        try {
+            sr = Signature.getInstance("SHA1withDSA");
+
+            Admin.provider = sr.getProvider();
+            Security.addProvider(Admin.provider);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public static Admin getAdmin(){
         if(admin == null){ admin = new Admin(); return admin;}
         else return admin;
     }
-    public static BouncyCastleProvider getProvider() {
-        return provider;
+    public static Provider getProvider() {
+
+        return Admin.provider;
     }
 
 
